8c2cbca5fe11302719ac785a057ef68bd28767e3
AhnSoo han <ahnsoohan@AhnSooui-MacBook-Air.local>
2015-11-14 18:07:39 +0900

    [#9] Modified unicorn/production.rb.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -14,3 +14,20 @@ if env == "production"
   stderr_path '/home/myuser/myapp_deployed/current/log/unicorn.stderr.log'
   stdout_path '/home/myuser/myapp_deployed/current/log/unicorn.stdout.log'
 end
+
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
