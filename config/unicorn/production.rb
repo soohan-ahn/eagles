2b77741c4975c0c661cb9269c2f96ba36485e1c3
AhnSoo han <ahnsoohan@AhnSooui-MacBook-Air.local>
2015-11-14 18:39:23 +0900

    [#9] Modified the path for working dir typo.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -26,7 +26,7 @@ stderr_path "log/unicorn.log"
 stdout_path "log/unicorn.log"
 
 # Set master PID location
-pid "#{app_path}tmp/pids/unicorn.pid"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #before_fork do |server, worker|
 #  ActiveRecord::Base.connection.disconnect!
