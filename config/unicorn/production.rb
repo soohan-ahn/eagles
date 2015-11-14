17d045c6c4d909d679b65d9aa728d3f8ae5e213a
AhnSoo han <ahnsoohan@AhnSooui-MacBook-Air.local>
2015-11-14 18:10:32 +0900

    [#9] Modified unicorn/production.rb.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,21 +1,6 @@
 #pidをフルパスで指定する
 pid "/home/deploy/eagles/shared/tmp/pids/unicorn.pid"
 
-# Production specific settings
-if env == "production"
-  # Help ensure your application will always spawn in the symlinked
-  # "current" directory that Capistrano sets up.
-  working_directory '/home/myuser/myapp_deployed/current'
-  
-  # feel free to point this anywhere accessible on the filesystem
-  user 'myuser', 'mygroup'
-  shared_path = '/home/myuser/myapp_deployed/current'
-  
-  stderr_path '/home/myuser/myapp_deployed/current/log/unicorn.stderr.log'
-  stdout_path '/home/myuser/myapp_deployed/current/log/unicorn.stdout.log'
-end
-
-
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
