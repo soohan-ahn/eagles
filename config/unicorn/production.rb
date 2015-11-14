ee410d5076e3af8e35a525db8450a9e5e1f9b492
AhnSoo han <ahnsoohan@AhnSooui-MacBook-Air.local>
2015-11-14 18:30:32 +0900

    [#9] Modified unicorn/production.rb.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -5,35 +5,42 @@
 app_path = '/home/deploy/eagles'
 app_shared_path = "#{app_path}/shared"
 
-worker_processes 5
-
-# 実態は symlink。
-# SIGUSR2 を送った時にこの symlink に対して
-# Unicorn のインスタンスが立ち上がる
-working_directory "#{app_path}/current/"
-
-listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
-
-stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
-stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
-
-pid "#{app_shared_path}/tmp/pids/unicorn.pid"
-
-# ダウンタイムをなくす
+# Set unicorn options
+worker_processes 1
 preload_app true
+timeout 20
+#listen "127.0.0.1:9000"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+# Spawn unicorn master worker for user apps (group: apps)
+#user 'apps', 'apps'
 
-  old_pid = "#{ server.config[:pid] }.oldbin"
-  unless old_pid == server.pid
-    begin
-      Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
+# Fill path to your app
+working_directory app_path
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+# Should be 'production' by default, otherwise use other env
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+# Log everything to one file
+stderr_path "log/unicorn.log"
+stdout_path "log/unicorn.log"
+
+# Set master PID location
+pid "tmp/pids/unicorn.pid"
+
+#before_fork do |server, worker|
+#  ActiveRecord::Base.connection.disconnect!
+#
+#  old_pid = "#{server.config[:pid]}.oldbin"
+#  if File.exists?(old_pid) && server.pid != old_pid
+#    begin
+#      Process.kill("QUIT", File.read(old_pid).to_i)
+#    rescue Errno::ENOENT, Errno::ESRCH
+#      # someone else did our job for us
+#    end
+#  end
+#end
+#
+#after_fork do |server, worker|
+#  ActiveRecord::Base.establish_connection
+#end
