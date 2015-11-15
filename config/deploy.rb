6e1e734d3e9efa1442843cf30a86b5ae2acc5028
AhnSoo han <ahnsoohan@AhnSooui-MacBook-Air.local>
2015-11-15 22:41:13 +0900

    [#9] Modified the deploy branch to matser.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :scm_username, :user
 set :scm_password, :password
 set :repo_url, "git://github.com/soohanboys/eagles.git"
 set :deploy_to, "/home/deploy/eagles"
-set :branch, "9-capistrano"
+set :branch, "master"
 
 set :default_shell, :bash
 set :rvm_type, :system
