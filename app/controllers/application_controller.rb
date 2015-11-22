e07f057efd7042a17d64102300387acacf6d7f20
Ahn-Soohan Home <soohanboys@gmail.com>
2015-11-22 10:19:25 +0900

    [#18] Remove pry and use session_helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  # Returns the current logged-in user (if any).
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
   def log_in(user)
     session[:user_id] = user.id
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root
+    redirect_to root_path
   end
 end
