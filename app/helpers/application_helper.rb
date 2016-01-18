ceefe0b6962bd3f8df29e64d383ac9032228e244
Ahn-Soohan Home <soohanboys@gmail.com>
2016-01-18 23:57:07 +0900

    [#30] Show result key word for view.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,6 +31,17 @@ module ApplicationHelper
     @inning_bat_records = at_bat_batter_record.where(batting_order: batting_order, inning: inning)
     if @inning_bat_records.present?
       @inning_bat_records.each do |at_bat_record|
+        @results.push Settings.result_code[at_bat_record.result_code]
+      end
+    end
+    return @results
+  end
+
+  def batting_result_code_numbers(at_bat_batter_record, batting_order, inning)
+    @results = [ ]
+    @inning_bat_records = at_bat_batter_record.where(batting_order: batting_order, inning: inning)
+    if @inning_bat_records.present?
+      @inning_bat_records.each do |at_bat_record|
         @results.push at_bat_record.result_code
       end
     end
