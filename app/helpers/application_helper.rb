68ed37ca7cc41c9f1b1bfad836d976504a2962d6
Ahn-Soohan Home <soohanboys@gmail.com>
2016-01-18 23:50:11 +0900

    [#30] Modified the game detail record edit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,7 +31,7 @@ module ApplicationHelper
     @inning_bat_records = at_bat_batter_record.where(batting_order: batting_order, inning: inning)
     if @inning_bat_records.present?
       @inning_bat_records.each do |at_bat_record|
-        @results.push Settings.result_code[at_bat_record.result_code]
+        @results.push at_bat_record.result_code
       end
     end
     return @results
