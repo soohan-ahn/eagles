2e9118a097e29db147354e22bcc1661508a415ae
Ahn-Soohan Home <soohanboys@gmail.com>
2016-11-26 00:32:55 +0900

    Bug fix for the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get "/seasons/:year" => "seasons#show"
+  get "seasons/:year" => "seasons#show"
 
   get    "login"   => "sessions#new"
   post   "login"   => "sessions#create"
