a0fb96ce38801166b5634b4dc0439a5c5d0d4386
Ahn-Soohan Home <soohanboys@gmail.com>
2016-12-27 19:56:46 +0900

    Korean on the plyaer_detail_records.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -6,6 +6,6 @@ module PlayersHelper
 	def date_and_team_of_game(game_id)
 		game = Game.find(game_id)
 		vs_team = (game.home_team == "Tokyo Eagles") ? game.away_team : game.home_team
-		game.game_start_time.strftime("%F") + vs_team
+		game.game_start_time.strftime("%F") + "\n" + vs_team
 	end
 end
