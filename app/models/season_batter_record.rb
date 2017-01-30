5f853862609a534cc22d98916b98da6fac76a962
Ahn-Soohan Home <soohanboys@gmail.com>
2017-01-30 18:25:50 +0900

    Make common method module for batter.

--- app/models/season_batter_record.rb
+++ app/models/season_batter_record.rb
@@ -1,5 +1,6 @@
 class SeasonBatterRecord < ActiveRecord::Base
-	belongs_to :player
+  belongs_to :player
+  include BatterCommonMethods
 
   def self.refresh_season_records(year_to_update = nil)
     year_range = (year_to_update) ? year_to_update..year_to_update : Settings.start_year..Date.today.year
@@ -75,34 +76,8 @@ class SeasonBatterRecord < ActiveRecord::Base
       is_regular_plate_appearance_satisfied: is_regular_plate_appearance_satisfied,
       plate_appearence: @plate_appearence,
     }
-    @non_update_columns_in_loop = [
-      "id",
-      "batting_average",
-      "on_base_percentage",
-      "slugging_percentage",
-      "ops",
-      "created_at",
-      "updated_at",
-    ]
-    SeasonBatterRecord.column_names.each do |column|
-      if !@non_update_columns_in_loop.include?(column) and @return_hash[column.to_sym].nil?
-        @return_hash[column.to_sym] = player_game_batter_records.sum(column.to_sym)
-      end
-    end
 
-    @batting_average = (@return_hash[:at_bat] > 0) ? (@return_hash[:total_hits].to_f / @return_hash[:at_bat]) : 0.0
-    @return_hash[:batting_average] = "%.3f" % @batting_average
-
-    @total_on_base = @return_hash[:total_hits] + @return_hash[:base_on_ball] + @return_hash[:hit_by_pitched_ball]
-    @on_base_percentage = (@return_hash[:plate_appearence] > 0) ? (@total_on_base.to_f / @return_hash[:plate_appearence]) : 0.0
-    @return_hash[:on_base_percentage] = "%.3f" % @on_base_percentage
-
-    @slugging_percentage = @return_hash[:one_base_hit] + (@return_hash[:two_base_hit] * 2) + (@return_hash[:three_base_hit] * 3) + (@return_hash[:home_run] * 4)
-    @slugging_percentage = (@return_hash[:at_bat] > 0) ? (@slugging_percentage.to_f / @return_hash[:at_bat]) : 0
-    @return_hash[:slugging_percentage] = "%.3f" % @slugging_percentage
-
-    @return_hash[:ops] = "%.3f" % (@slugging_percentage + @on_base_percentage)
-
+    summarize_from_records(@return_hash, player_game_batter_records)
     @return_hash
   end
 
--- app/models/total_batter_record.rb
+++ app/models/total_batter_record.rb
@@ -1,5 +1,6 @@
 class TotalBatterRecord < ActiveRecord::Base
   belongs_to :player
+  include BatterCommonMethods
 
   def self.summarize
     Player.all.each do |current_player|
@@ -22,44 +23,17 @@ class TotalBatterRecord < ActiveRecord::Base
   end
 
   def self.batter_records_of_player(current_player)
-    @season_batter_records = current_player.season_batter_records
-    @plate_appearence = @season_batter_records.sum(:plate_appearence)
-    is_regular_plate_appearance_satisfied = (200 <= @plate_appearence) ? true : false
+    season_batter_records = current_player.season_batter_records
+    plate_appearence = season_batter_records.sum(:plate_appearence)
+    is_regular_plate_appearance_satisfied = (200 <= plate_appearence) ? true : false
 
     @return_hash = {
       player_id: current_player.id,
       is_regular_plate_appearance_satisfied: is_regular_plate_appearance_satisfied,
-      played_game: @season_batter_records.count,
-      plate_appearence: @plate_appearence,
+      played_game: season_batter_records.count,
+      plate_appearence: plate_appearence,
     }
-    @non_update_columns_in_loop = [
-      "id",
-      "batting_average",
-      "on_base_percentage",
-      "slugging_percentage",
-      "ops",
-      "created_at",
-      "updated_at",
-    ]
-    TotalBatterRecord.column_names.each do |column|
-      if !@non_update_columns_in_loop.include?(column) and @return_hash[column.to_sym].nil?
-        @return_hash[column.to_sym] = @season_batter_records.sum(column.to_sym)
-      end
-    end
-
-    @batting_average = (@return_hash[:at_bat] > 0) ? (@return_hash[:total_hits].to_f / @return_hash[:at_bat]) : 0.0
-    @return_hash[:batting_average] = "%.3f" % @batting_average
-
-    @total_on_base = @return_hash[:total_hits] + @return_hash[:base_on_ball] + @return_hash[:hit_by_pitched_ball]
-    @on_base_percentage = (@return_hash[:plate_appearence] > 0) ? (@total_on_base.to_f / @return_hash[:plate_appearence]) : 0.0
-    @return_hash[:on_base_percentage] = "%.3f" % @on_base_percentage
-
-    @slugging_percentage = @return_hash[:one_base_hit] + (@return_hash[:two_base_hit] * 2) + (@return_hash[:three_base_hit] * 3) + (@return_hash[:home_run] * 4)
-    @slugging_percentage = (@return_hash[:at_bat] > 0) ? (@slugging_percentage.to_f / @return_hash[:at_bat]) : 0
-    @return_hash[:slugging_percentage] = "%.3f" % @slugging_percentage
-
-    @return_hash[:ops] = "%.3f" % (@slugging_percentage + @on_base_percentage)
-
+    summarize_from_records(@return_hash, season_batter_records)
     @return_hash
   end
 end
