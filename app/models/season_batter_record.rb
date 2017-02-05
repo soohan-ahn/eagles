e5aa5a91d100ddffee08daf25e9eee86e34f146b
Ahn-Soohan Home <soohanboys@gmail.com>
2017-02-05 23:19:49 +0900

    Revert "Merge pull request #79 from soohanboys/tmp/game_batter_record"
    This reverts commit e2207860832b66dc84c7f2eabee9384a094d0dbc, reversing
    changes made to 5c83777e001c6319ca8e732d94396b6a3c07ecaa.

--- app/models/season_batter_record.rb
+++ app/models/season_batter_record.rb
@@ -2,66 +2,52 @@ class SeasonBatterRecord < ActiveRecord::Base
   belongs_to :player
   include BatterCommonMethods
 
-	def self.refresh_season_records(year_to_update = nil)
-		year_range = (year_to_update) ? year_to_update..year_to_update : Settings.start_year..Date.today.year
-		year_range.each do |year|
-			players = Player.all
+  def self.summarize(year_to_update = nil)
+    year_range = (year_to_update) ? year_to_update..year_to_update : Settings.start_year..Date.today.year
+    year_range.each do |year|
+      players = Player.all
 
-			players.each do |player|
-				batter_records_of_season = SeasonBatterRecord.find_by(
-					year: year, player_id: player.id
-				)
+      players.each do |player|
+        batter_records_of_season = SeasonBatterRecord.find_by(
+          year: year, player_id: player.id
+        )
 
-				refreshed_records = SeasonBatterRecord.batter_records_of_player(player: player, year: year)
+        refreshed_records = SeasonBatterRecord.batter_records_of_player(player: player, year: year)
 
-				ActiveRecord::Base.transaction do
-					if batter_records_of_season
-						# Update
-						batter_records_of_season.update(refreshed_records)
-					elsif refreshed_records[:played_game] > 0
-						# New
-						new_batter_records = SeasonBatterRecord.new(refreshed_records)
-						return false unless new_batter_records.save
-					end
-				end
-
-			end
-		end
-	end
+        if batter_records_of_season
+          # Update
+          refreshed_records[:created_at] = batter_records_of_season[:created_at]
+          refreshed_records[:updated_at] = Time.now()
+          batter_records_of_season.update!(refreshed_records)
+        elsif refreshed_records[:played_game] > 0
+          # New
+          new_batter_records = SeasonBatterRecord.new(refreshed_records)
+          return false unless new_batter_records.save
+        end
+      end
+    end
+  end
 
   def self.batter_records_of_player(params)
   	player = params[:player]
-    game_count = (params[:year]) ? Game.by_year(params[:year]).count : Game.all.count
-    is_regular_plate_appearance_satisfied = (game_count * Settings.regular_plate_appearance_rate <= player.plate_appearence(params[:year]))
 
-    {
+    game_id_of_the_years = (params[:year]) ? Game.by_year(params[:year]).pluck(:id) : Game.all.pluck(:id)
+    player_game_batter_records = player.game_batter_records.where(game_id: game_id_of_the_years)
+
+    plate_appearence = player_game_batter_records.sum(:plate_appearence)
+    regular_plate_appearance_rate = game_id_of_the_years.count * Settings.regular_plate_appearance_rate
+    is_regular_plate_appearance_satisfied = (regular_plate_appearance_rate <= plate_appearence)
+
+    return_hash = {
       player_id: player.id,
       year: params[:year],
-      played_game: player.game_batter_records_this_year(params[:year]).count,
-      plate_appearence: player.plate_appearence(params[:year]),
-      at_bat: player.at_bat(params[:year]),
-      total_hits: player.total_hits(params[:year]),
-      one_base_hit: player.retrieve_at_bat_batter_records("one_base_hit", params[:year]),
-      two_base_hit: player.retrieve_at_bat_batter_records("two_base_hit", params[:year]),
-      three_base_hit: player.retrieve_at_bat_batter_records("three_base_hit", params[:year]),
-      home_run: player.retrieve_at_bat_batter_records("home_run", params[:year]),
-      strike_out: player.retrieve_at_bat_batter_records("strike_out", params[:year]),
-      base_on_ball: player.retrieve_at_bat_batter_records("base_on_ball", params[:year]),
-      hit_by_pitched_ball: player.retrieve_at_bat_batter_records("hit_by_pitched_ball", params[:year]),
-      rbi: player.retrieve_game_batter_records("rbi", params[:year]),
-      run: player.retrieve_game_batter_records("run", params[:year]),
-      steal: player.retrieve_game_batter_records("steal", params[:year]),
-      steal_caught: player.retrieve_game_batter_records("steal_caught", params[:year]),
-      sacrifice_hit: player.retrieve_at_bat_batter_records("sacrifice_bunts", params[:year]),
-      sacrifice_fly: player.retrieve_at_bat_batter_records("sacrifice_flys", params[:year]),
-      double_play: player.retrieve_at_bat_batter_records("double_plays", params[:year]),
-      batting_average: player.batting_average(params[:year]),
-      on_base_percentage: player.on_base_percentage(params[:year]),
-      slugging_percentage: player.slugging_percentage(params[:year]),
-      ops: player.ops(params[:year]),
-      is_regular_plate_appearance_satisfied: (is_regular_plate_appearance_satisfied) ? true : false,
-      on_base_by_error: player.retrieve_at_bat_batter_records("on_base_by_error", params[:year]),
+      played_game: player_game_batter_records.count,
+      is_regular_plate_appearance_satisfied: is_regular_plate_appearance_satisfied,
+      plate_appearence: plate_appearence,
     }
+
+    summarize_from_records(return_hash, player_game_batter_records)
+    return_hash
   end
 
   def self.batter_records(params)
